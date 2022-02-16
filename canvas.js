7e9691269366c05f2672a6262a778a7bab17a322
ZeekBeakCreek <89820141+ZeekBeakCreek@users.noreply.github.com>
2022-02-16 16:09:30 -0700

    Add files via upload

---
+++ canvas.js
@@ -0,0 +1,348 @@
+window.onload = () => {
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
+
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  // outside darker green
+  ctx.fillStyle = "hsl(110, 48%, 37%)";
+  ctx.fillRect(275, 50, 25, 25);
+
+  // outside lighter green
+  ctx.fillStyle = "hsl(126, 68%, 46%)";
+  ctx.fillRect(300, 50, 25, 25);
+
+  // darker green
+  ctx.fillStyle = "hsl(110, 48%, 37%)";
+  ctx.fillRect(300, 75, 25, 25);
+
+  // lighter green
+  ctx.fillStyle = "hsl(126, 68%, 46%)";
+  ctx.fillRect(325, 75, 25, 25);
+
+  // 6 down repeat darker green
+  ctx.fillStyle = "hsl(110, 48%, 37%)";
+  ctx.fillRect(325, 100, 25, 25);
+
+  ctx.fillStyle = "hsl(110, 48%, 37%)";
+  ctx.fillRect(325, 125, 25, 25);
+
+  ctx.fillStyle = "hsl(110, 48%, 37%)";
+  ctx.fillRect(325, 150, 25, 25);
+
+  ctx.fillStyle = "hsl(110, 48%, 37%)";
+  ctx.fillRect(325, 175, 25, 25);
+
+  ctx.fillStyle = "hsl(110, 48%, 37%)";
+  ctx.fillRect(325, 200, 25, 25);
+
+  ctx.fillStyle = "hsl(110, 48%, 37%)";
+  ctx.fillRect(325, 225, 25, 25);
+
+  // 6 down repeat lighter green
+  ctx.fillStyle = "hsl(126, 68%, 46%)";
+  ctx.fillRect(350, 100, 25, 25);
+
+  ctx.fillStyle = "hsl(126, 68%, 46%)";
+  ctx.fillRect(350, 125, 25, 25);
+
+  ctx.fillStyle = "hsl(126, 68%, 46%)";
+  ctx.fillRect(350, 150, 25, 25);
+
+  ctx.fillStyle = "hsl(126, 68%, 46%)";
+  ctx.fillRect(350, 175, 25, 25);
+
+  ctx.fillStyle = "hsl(126, 68%, 46%)";
+  ctx.fillRect(350, 200, 25, 25);
+
+  ctx.fillStyle = "hsl(126, 68%, 46%)";
+  ctx.fillRect(350, 225, 25, 25);
+
+  // dark green only
+  ctx.fillStyle = "hsl(110, 48%, 37%)";
+  ctx.fillRect(300, 250, 25, 25);
+
+  ctx.fillStyle = "hsl(110, 48%, 37%)";
+  ctx.fillRect(275, 275, 25, 25);
+
+  ctx.fillStyle = "hsl(110, 48%, 37%)";
+  ctx.fillRect(250, 300, 25, 25);
+
+  // 1
+  ctx.fillStyle = "hsl(110, 48%, 37%)";
+  ctx.fillRect(225, 325, 25, 25);
+
+  ctx.fillStyle = "hsl(110, 48%, 37%)";
+  ctx.fillRect(200, 325, 25, 25);
+
+  ctx.fillStyle = "hsl(110, 48%, 37%)";
+  ctx.fillRect(175, 325, 25, 25);
+
+  ctx.fillStyle = "hsl(110, 48%, 37%)";
+  ctx.fillRect(150, 325, 25, 25);
+
+  ctx.fillStyle = "hsl(110, 48%, 37%)";
+  ctx.fillRect(125, 325, 25, 25);
+
+  // 6
+  ctx.fillStyle = "hsl(110, 48%, 37%)";
+  ctx.fillRect(100, 325, 25, 25);
+
+  ctx.fillStyle = "hsl(110, 48%, 37%)";
+  ctx.fillRect(75, 300, 25, 25);
+
+  ctx.fillStyle = "hsl(110, 48%, 37%)";
+  ctx.fillRect(50, 275, 25, 25);
+
+  // light green only
+  ctx.fillStyle = "hsl(126, 68%, 46%)";
+  ctx.fillRect(325, 250, 25, 25);
+
+  ctx.fillStyle = "hsl(126, 68%, 46%)";
+  ctx.fillRect(325, 275, 25, 25);
+
+  ctx.fillStyle = "hsl(126, 68%, 46%)";
+  ctx.fillRect(300, 275, 25, 25);
+
+  ctx.fillStyle = "hsl(126, 68%, 46%)";
+  ctx.fillRect(300, 300, 25, 25);
+
+  ctx.fillStyle = "hsl(126, 68%, 46%)";
+  ctx.fillRect(275, 300, 25, 25);
+
+  ctx.fillStyle = "hsl(126, 68%, 46%)";
+  ctx.fillRect(275, 325, 25, 25);
+
+  ctx.fillStyle = "hsl(126, 68%, 46%)";
+  ctx.fillRect(250, 325, 25, 25);
+
+  // 1
+  ctx.fillStyle = "hsl(126, 68%, 46%)";
+  ctx.fillRect(225, 350, 25, 25);
+
+  ctx.fillStyle = "hsl(126, 68%, 46%)";
+  ctx.fillRect(200, 350, 25, 25);
+
+  ctx.fillStyle = "hsl(126, 68%, 46%)";
+  ctx.fillRect(175, 350, 25, 25);
+
+  ctx.fillStyle = "hsl(126, 68%, 46%)";
+  ctx.fillRect(150, 350, 25, 25);
+
+  ctx.fillStyle = "hsl(126, 68%, 46%)";
+  ctx.fillRect(125, 350, 25, 25);
+
+  // 6
+  ctx.fillStyle = "hsl(126, 68%, 46%)";
+  ctx.fillRect(100, 350, 25, 25);
+
+  ctx.fillStyle = "hsl(126, 68%, 46%)";
+  ctx.fillRect(75, 325, 25, 25);
+
+  ctx.fillStyle = "hsl(126, 68%, 46%)";
+  ctx.fillRect(50, 300, 25, 25);
+
+  // light pink
+  ctx.fillStyle = "hsl(6, 64%, 67%)";
+  ctx.fillRect(75, 275, 25, 25);
+
+  ctx.fillStyle = "hsl(6, 64%, 67%)";
+  ctx.fillRect(100, 275, 25, 25);
+
+  // 1 
+  ctx.fillStyle = "hsl(6, 64%, 67%)";
+  ctx.fillRect(100, 300, 25, 25);
+
+  ctx.fillStyle = "hsl(6, 64%, 67%)";
+  ctx.fillRect(125, 300, 25, 25);
+
+  ctx.fillStyle = "hsl(6, 64%, 67%)";
+  ctx.fillRect(150, 300, 25, 25);
+
+  ctx.fillStyle = "hsl(6, 64%, 67%)";
+  ctx.fillRect(175, 300, 25, 25);
+
+  ctx.fillStyle = "hsl(6, 64%, 67%)";
+  ctx.fillRect(200, 300, 25, 25);
+
+  // 6 
+  ctx.fillStyle = "hsl(6, 64%, 67%)";
+  ctx.fillRect(225, 300, 25, 25);
+
+  ctx.fillStyle = "hsl(6, 64%, 67%)";
+  ctx.fillRect(225, 275, 25, 25);
+
+  ctx.fillStyle = "hsl(6, 64%, 67%)";
+  ctx.fillRect(250, 275, 25, 25);
+
+  ctx.fillStyle = "hsl(6, 64%, 67%)";
+  ctx.fillRect(275, 250, 25, 25);
+
+  ctx.fillStyle = "hsl(6, 64%, 67%)";
+  ctx.fillRect(275, 225, 25, 25);
+
+  // 1 
+  ctx.fillStyle = "hsl(6, 64%, 67%)";
+  ctx.fillRect(300, 225, 25, 25);
+
+  ctx.fillStyle = "hsl(6, 64%, 67%)";
+  ctx.fillRect(300, 200, 25, 25);
+
+  ctx.fillStyle = "hsl(6, 64%, 67%)";
+  ctx.fillRect(300, 175, 25, 25);
+
+  ctx.fillStyle = "hsl(6, 64%, 67%)";
+  ctx.fillRect(300, 150, 25, 25);
+
+  ctx.fillStyle = "hsl(6, 64%, 67%)";
+  ctx.fillRect(300, 125, 25, 25);
+
+  // 6
+  ctx.fillStyle = "hsl(6, 64%, 67%)";
+  ctx.fillRect(300, 100, 25, 25);
+
+  ctx.fillStyle = "hsl(6, 64%, 67%)";
+  ctx.fillRect(275, 100, 25, 25);
+
+  ctx.fillStyle = "hsl(6, 64%, 67%)";
+  ctx.fillRect(275, 75, 25, 25);
+
+  // juicy fruit 
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(250, 75, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(250, 100, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(225, 100, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(225, 100, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(225, 125, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(200, 125, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(200, 150, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(175, 150, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(175, 175, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(150, 175, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(150, 200, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(125, 200, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(125, 225, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(100, 225, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(100, 250, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(75, 250, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(125, 275, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(150, 275, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(175, 275, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(200, 275, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(150, 250, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(175, 250, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(200, 250, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(225, 250, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(225, 225, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(200, 225, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(250, 225, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(250, 250, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(150, 225, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(175, 200, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(200, 200, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(225, 200, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(250, 200, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(275, 200, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(200, 175, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(250, 175, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(275, 175, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(275, 150, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(275, 125, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(250, 150, 25, 25);
+
+  ctx.fillStyle = "hsl(357, 90%, 47%)";
+  ctx.fillRect(225, 150, 25, 25);
+
+  // seeds
+  ctx.fillStyle = 'hsl(357, 0%, 0%)'
+  ctx.fillRect(250, 125, 25, 25);
+
+  ctx.fillStyle = 'hsl(357, 0%, 0%)'
+  ctx.fillRect(225, 175, 25, 25);
+
+  ctx.fillStyle = 'hsl(357, 0%, 0%)'
+  ctx.fillRect(175, 225, 25, 25);
+
+  ctx.fillStyle = 'hsl(357, 0%, 0%)'
+  ctx.fillRect(125, 250, 25, 25);
+};
+
